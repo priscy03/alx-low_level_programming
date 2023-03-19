4ba495be6b14a97c4cc90db45dd33b525444f03c
Priscilla <adwoa.priscilla@gmail.com>
2023-03-18 19:09:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main-A fuction that prints exactly followed by new line
+*Return:always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}	
