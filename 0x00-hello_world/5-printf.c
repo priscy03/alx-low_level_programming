8b8246189a20c252aab98461e343342302ced2b8
Priscilla <adwoa.priscilla@gmail.com>
2023-03-18 20:18:23 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- A c program that prints exactly a line with printf
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
